79067f7b5e6fb92b256cb1b3c2302684d067b2e0
carlloshenrry <38138832+carlloshenrry@users.noreply.github.com>
2019-08-29 11:03:01 -0300

    Aula teorica 3. Excessao e revisao de polimorfismo

---
+++ Execessoes.java
@@ -0,0 +1,31 @@
+//criando objteto
+public abstract class Aviao{
+	//Atributos
+	public String cor;
+	public String nomeAviao;
+	
+	//Metodos
+	public void projetoAviao(){
+		///syste...("Projeto");
+	}
+	
+	//Interface Abstrata
+	public abstract void atrbiuirCor (String cor);
+	public abstract void atribuirNome(String Nome);
+}
+
+public class Comp.Ae extends Aviao{
+	//...
+	public void atribuirCor(String Cor)
+	{
+		try{
+			
+		}//se ocorrer exceção usa o cath
+	cath(Exception e}//intancia o e ,para ser um objeto/varaivel e recebe detalhes das coisas que ocorreu no erro
+	{
+		///System...("Problema..."+e.getMessage());
+		///if(e.getmessage=="float")
+		{}
+	}
+	
+	}
---
+++ polimorfismo.java
@@ -0,0 +1,26 @@
+public abstract class Aviao{
+	//Atributos
+	public String cor;
+	public String nomeAviao;
+	
+	//Metodos
+	public void projetoAviao(){
+		//syste...("Projeto");
+	}
+	
+	//Interface Abstrata
+	public abstract void atrbiuirCor (String cor);
+	public abstract void atribuirNome(String Nome);
+}
+
+public class Comp.Ae extends Aviao{
+	//...
+	public void atribuirCor(String Cor)
+	{
+		super.cor="Laranja";
+		//if(...)
+		//for(...)
+	}
+public void atribuirNome (String Nome){
+	//se no quiser entrada implementa vazia
+}
